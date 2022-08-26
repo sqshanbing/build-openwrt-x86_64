a49ecd965be9bc5345d0d5a4a71334bd01c305ac
sqshanbing <33117168+sqshanbing@users.noreply.github.com>
2022-08-26 17:53:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 echo 'src-git small8 https://github.com/kenzok8/small-package
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
